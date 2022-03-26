a13f6b3b4664d13edc14de922d01f1cd9291e7c4
kavinkavin16 <kavinnares@gmail.com>
2022-03-27 04:05:05 +0530

    modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello World🚀</div>;
-}
+// function App() {
+//   return <div className="App">Hello World🚀</div>;
+// }
+
+const App = () => <h1 className="App">Hello World🚀</h1>;
 
 export default App;
